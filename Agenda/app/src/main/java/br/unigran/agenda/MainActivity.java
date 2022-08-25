401e4096959505ab6896cc81b1bef7c2d5209dcc
matheus ribeiro santana <802.243@alunos.unigran.br>
2022-08-24 21:15:12 -0400

    mudando para a main

--- Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
+++ Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
@@ -3,12 +3,41 @@ package br.unigran.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText  nome;
+    EditText  telefone;
+    ListView  listagem;
+    List<Contato>dados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nome = findViewById(R.id.nameID);
+        telefone = findViewById(R.id.phoneID);
+        listagem = findViewById(R.id.listID);
+        dados = new ArrayList();
+        ArrayAdapter adapter =
+                new ArrayAdapter
+
+    }
+
+    public void salvar(View view){
+        Contato contato =new Contato();
+        contato.setNome(nome.getText().toString());
+        contato.setTelefone(telefone.getText().toString());
+        dados.add(contato);
+        Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+
+
     }
 }
