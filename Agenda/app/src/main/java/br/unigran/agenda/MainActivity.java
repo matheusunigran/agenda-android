36939cf124400f738b635bf315f97ebd297b0973
matheus ribeiro santana <802.243@alunos.unigran.br>
2022-08-30 21:51:33 -0400

    Criando banco de dados

--- Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
+++ Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
@@ -2,6 +2,7 @@ package br.unigran.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,23 +15,33 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.unigran.bancoDados.DBHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText  nome;
     EditText  telefone;
     ListView  listagem;
     List<Contato>dados;
+    SQLiteDatabase conexao;
+    DBHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //banco de dados
+        db = new DBHelper(this);
+
+
         setContentView(R.layout.activity_main);
         nome = findViewById(R.id.nameID);
         telefone = findViewById(R.id.phoneID);
         listagem = findViewById(R.id.listID);
         dados = new ArrayList();
         ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dados);
-
         listagem.setAdapter(adapter);
+
+
     }
 
     public void salvar(View view){
@@ -38,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         contato.setNome(nome.getText().toString());
         contato.setTelefone(telefone.getText().toString());
         dados.add(contato);
+        db.inserir(contato, db);
         Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+
+
     }
 }
---
+++ Agenda/app/src/main/java/br/unigran/bancoDados/DBHelper.java
@@ -0,0 +1,36 @@
+package br.unigran.bancoDados;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import br.unigran.agenda.Contato;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "BancoAgenda", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table agenda(" +
+                "id integer primary key autoincrement," +
+                "nome varchar(150)," +
+                "telefone varchar(15))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+    public void inserir(Contato contato, DBHelper db){
+        SQLiteDatabase conexao = db.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+        valores.put("nome", contato.getNome());
+        valores.put("telefone", contato.getTelefone());
+        conexao.insertOrThrow("Agenda", null, valores);
+        conexao.close();
+    }
+}
